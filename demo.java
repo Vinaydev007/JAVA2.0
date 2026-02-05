13efdae7793a9804c175e3edb56fd4a7bf9495e6
Vinaydev007 <vinaybairavarapu@gmail.com>
2026-02-05 16:06:37 +0530

    05-02-2026

--- demo.java
+++ demo.java
@@ -1,19 +1,22 @@
 class car{
        String brand;
        String model;
-       int Cost;
+       long cost;
        public void speed(int speed){
-          System.out.println("it's spped range is upto"+ speed);
+          System.out.println("it's speed range is upto:"+ speed);
+         //  System.out.println("cost of the vehicle:"+);
        }
 }  
 
 public class demo{
    
     public static void main(String []args){
-         int speed=300;
+      int speed=300;
       car obj=new car();
       obj.brand="XUV";
       obj.model="2026";
+      obj.cost=10000000l;
       obj.speed(speed);
+      System.out.println(obj.brand+" "+obj.model+" "+obj.cost);
     }
 }
